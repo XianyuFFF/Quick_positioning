ad283e7b4ced13b5a266ebc03fcf9e073f476408
icicle4 <icicle4@icloud.com>
2019-03-09 21:34:24 +0800

    init

---
+++ Code/PyDeepCC/Compute_L0_features.py
@@ -0,0 +1,8 @@
+from .config_default import configs
+import os
+
+
+
+command = 'python3 embed_detections.py --experiment_root {} --dataset_path {} --detections_path {} --filename {}'
+
+os.system()
---
+++ Code/PyDeepCC/config_default.py
@@ -0,0 +1,6 @@
+configs = {
+    'dataset_path': '/home/fyq/Documents/Datasets/DukeMTMC/',
+    'detections_path': '',
+    'file_name': '',
+    'expirement_root': ''
+}
